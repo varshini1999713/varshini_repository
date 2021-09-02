0f947111c9e72333cde45c1951fc0f2a57e59127
varshini1999713 <88525161+varshini1999713@users.noreply.github.com>
2021-09-02 17:35:15 +0530

    Add files via upload

---
+++ arranging_dict.py
@@ -0,0 +1,10 @@
+num = int(input())
+res = {}
+print('{',end = '')
+for i in range(0, num+1):
+    if i==num:
+        print('{0}:{1}'.format(i, i*i), end='')
+    else:
+        print('{0}:{1}, '.format(i, i * i), end='')
+
+print('}')
